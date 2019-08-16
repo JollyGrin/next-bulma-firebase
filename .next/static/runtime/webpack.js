bc6b30c4f28c0145f572d3f602cbd75676e37905
jollygrin <jollygrin@protonmail.com>
2019-08-16 13:28:10 +0200

    update

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2b6b4a2f76db2a6c7aeb";
+/******/ 	var hotCurrentHash = "4ccc330b5e10dff755b2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { withRouter } from 'next/router';
 import Link from 'next/link';
+import { firestore } from '../lib/db';
 import { fetchDocumentFromCollectionByFieldName } from '../lib/utility';
 import DateFormatter from '../components/DateFormatter';
 
@@ -9,18 +10,35 @@ class Blog extends Component {
     blog: null
   };
 
-  componentDidMount() {
-    // find the blog
+  constructor(props) {
+    super(props);
+    this.blogSlug = this.props.router.query.slug;
+    this.collectionName = 'blogs';
+  }
+
+  blogListener = () => {
     fetchDocumentFromCollectionByFieldName({
-      collectionName: 'blogs',
+      collectionName: this.collectionName,
       fieldName: 'slug',
-      value: this.props.router.query.slug
+      value: this.blogSlug
     }).then(blog => {
-      // update the state
-      console.log(blog);
       this.setState({ blog });
     });
-    // update the state
+  };
+
+  componentDidMount() {
+    this.blogListener();
+
+    this.unsubscribe = firestore
+      .collection(this.collectionName)
+      .where('slug', '==', this.blogSlug)
+      .onSnapshot(this.blogListener, error => {
+        console.log(`firestone snapshot error ${error}`);
+      });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   render() {
